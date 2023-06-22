5eb813f39aaed72b1e1e54534a8da66c79b96d10
brijean <bjean09@gmail.com>
2023-06-22 16:25:29 -0400

    commit for demo

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "aws-vm" {
   source  = "intel/aws-vm/intel"
   version = "1.0.3"
   key_name = "aws-key-1"
-  #instance_type = "t2.micro"
+  instance_type = "t2.micro"
    tags = {
     Name = "Intel Hashicorp Demo"
   }
